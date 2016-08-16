b17837564405adbf9cb058b5be752dba2da0d05f
Johnsmith0508 <waldmanl@dynalogic.org>
2016-08-15 20:33:59 -0400

    initial commit v2

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>OverSim</title>
+  </head>
+  <body>
+    <div style="position:absolute;height:30px;width:40px;background-color:#f00">
+      @yield('content');
+    </div>
+  </body>
+</html>
